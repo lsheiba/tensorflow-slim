850451ff1d85d409d87ad108d042b6e2983db6d1
Leonard <lseiba@kuberlab.com>
2018-01-24 09:06:36 -0800

    added tar and mv

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -3,9 +3,12 @@ MY_WORKSPACE=test-projects
 MY_TOKEN=ce2135f0-9edd-4666-a8b2-56378b8165d2
 MY_MODEL=inception_v3
 MY_MODEL_VERSION=1.0.0
-
+PRETRAINED_CHECKPOINT_DIR=/notebooks
 
 
 curl -H “Authorization: Bearer ${MY_TOKEN}” \
   https://dev.kuberlab.io/api/v0.2/workspace/${MY_WORKSPACE}/mlmodels/${MY_MODEL}/versions/${MY_MODEL_VERSION}/download \
   > ${MY_MODEL}.tgz
+tar -xvzf ${MY_MODEL}.tgz
+mv ${MY_MODEL}.ckpt ${PRETRAINED_CHECKPOINT_DIR}/${MY_MODEL}.ckpt
+rm ${MY_MODEL}.tgz
